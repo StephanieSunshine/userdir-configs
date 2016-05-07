123e2a6eaf7e138a5b4e0869e2535c9444af6ac2
Stephanie Sunshine <StephanieSunshine@users.noreply.github.com>
2016-05-06 18:42:33 -0700

    Update install-packages.sh

--- install-packages.sh
+++ install-packages.sh
@@ -6,10 +6,10 @@ fi
 $SUDO apt-get install -y vim
 
 #Filter stuff
-$SUDO apt-get install -y multitail grc grcat
+$SUDO apt-get install -y multitail grc
 
 #Monitoring
 $SUDO apt-get install -y glances htop iotop lm-sensors ipmitool pv
 
 #Debug
-$SUDO apt-get install -y nmap tcp tcptraceroute tcpdump wireshark iperf speedtest-cli
+$SUDO apt-get install -y nmap tcptraceroute tcpdump wireshark iperf speedtest-cli
