40f71dc027acae26fe9c12af23b1d1ed1d8133ef
Stephanie Sunshine <StephanieSunshine@users.noreply.github.com>
2017-11-21 13:25:49 -0800

    Update install-packages.sh

--- install-packages.sh
+++ install-packages.sh
@@ -6,7 +6,7 @@ fi
 $SUDO apt-get install -y vim
 
 #Filter stuff
-$SUDO apt-get install -y multitail grc
+$SUDO apt-get install -y multitail grc source-highlight
 
 #Monitoring
 $SUDO apt-get install -y glances htop iotop lm-sensors ipmitool pv
